41039bf49bff07270138ac8274bf1e4fd78bc279
ufuknexasoft <133389543+ufuknexasoft@users.noreply.github.com>
2024-07-29 15:35:52 +0300

    downgrade

--- config/server.js
+++ config/server.js
@@ -1,17 +1,11 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
+  url: env("PUBLIC_URL", "https://strapi-backend-m1vd.onrender.com"),
   app: {
     keys: env.array("APP_KEYS", ["myKeyA", "myKeyB"]),
   },
   webhooks: {
     populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
-  },
-
-  url: env("PUBLIC_URL", "https://strapi-backend-m1vd.onrender.com"),
-
-  admin: {
-    url: "/admin", // Ensure this is correct
-    serveAdminPanel: true, // Make sure this is set to true
   },
 });
