a194f9d7f54969937b8c1acdb576f2e3b0103d2f
ufuknexasoft <133389543+ufuknexasoft@users.noreply.github.com>
2024-07-29 15:18:24 +0300

    minor changes

--- config/server.js
+++ config/server.js
@@ -7,4 +7,11 @@ module.exports = ({ env }) => ({
   webhooks: {
     populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
+
+  url: env("PUBLIC_URL", "https://strapi-backend-m1vd.onrender.com"),
+
+  admin: {
+    url: "/admin", // Ensure this is correct
+    serveAdminPanel: true, // Make sure this is set to true
+  },
 });
