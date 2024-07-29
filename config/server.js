cc97220ca800fe7731d264e03f47313a2d75d5ac
ufuknexasoft <133389543+ufuknexasoft@users.noreply.github.com>
2024-07-29 18:19:53 +0300

    sdasd

--- config/server.js
+++ config/server.js
@@ -1,10 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"), // This should be the hostname or IP address
-  port: env.int("PORT", 1337), // Use the appropriate port for your Strapi instance
-  url: env("PUBLIC_URL", "https://strapi-backend-m1vd.onrender.com"),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
     keys: env.array("APP_KEYS", ["myKeyA", "myKeyB"]),
   },
+  url: env("PUBLIC_URL", "https://strapi-backend-m1vd.onrender.com"),
   webhooks: {
     populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
