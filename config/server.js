6d05201e85dc7c99c8f5f59b16775168a3431b74
ufuknexasoft <133389543+ufuknexasoft@users.noreply.github.com>
2024-07-30 15:37:11 +0300

    url check

--- config/server.js
+++ config/server.js
@@ -4,7 +4,8 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array("APP_KEYS", ["myKeyA", "myKeyB"]),
   },
-
+  url: env("PUBLIC_URL", "https://strapi-backend-m1vd.onrender.com"),
+  nodeOptions: env("NODE_OPTIONS", "--max-old-space-size=2048"),
   webhooks: {
     populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
