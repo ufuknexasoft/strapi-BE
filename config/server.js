85511cd9d20f098b908da2cc553c3bf033f351ee
ufuknexasoft <133389543+ufuknexasoft@users.noreply.github.com>
2024-08-01 15:11:33 +0300

    sdsd

--- config/server.js
+++ config/server.js
@@ -1,10 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
+  host: env("HOST", "localhost"),
+  port: env.int("PORT", 8080),
   app: {
     keys: env.array("APP_KEYS", ["myKeyA", "myKeyB"]),
   },
-  url: env("PUBLIC_URL", "https://strapi-backend-m1vd.onrender.com"),
+
   nodeOptions: env("NODE_OPTIONS", "--max-old-space-size=2048"),
   webhooks: {
     populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
